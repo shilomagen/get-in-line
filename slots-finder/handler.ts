a9dfbf04a4735537483c4b5abc4a5955a7efa037
Shilo Magen <smangam@gmail.com>
2022-04-28 06:58:30 +0300

    Publish to sqs queue

--- slots-finder/handler.ts
+++ slots-finder/handler.ts
@@ -1,11 +1,23 @@
 import { SlotsFinder } from './src/slots-finder';
 import { HttpService, SessionCreator } from 'get-in-line-shared/dist';
+import { SendMessageCommand, SQSClient } from '@aws-sdk/client-sqs';
 
 const MAX_DAYS = 7;
 export const run = async (_event: any, _context: any) => {
-  const token = await SessionCreator.create()
+  const token = await SessionCreator.create();
   const httpService = new HttpService(token);
   const slotsFinder = new SlotsFinder(httpService);
-  const slots = await slotsFinder.find(MAX_DAYS)
-  console.log(slots)
+  const sqsClient = new SQSClient({ region: 'eu-central-1' });
+  const queueUrl = process.env.QUEUE_URL;
+  const slots = await slotsFinder.find(MAX_DAYS);
+
+  const publishes = slots.map(slot =>
+    sqsClient.send(new SendMessageCommand({
+      QueueUrl: queueUrl,
+      MessageBody: JSON.stringify(slot)
+    })));
+  
+  await Promise.all(publishes);
 };
+
+
